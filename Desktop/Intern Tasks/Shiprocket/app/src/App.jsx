bacde84bda5cd1b0c243eb6a1b4ed38ced527e06
SUBASH <subashvj78@gmail.com>
2025-02-04 10:23:11 +0530

    Cargo-Shipping Page

---
+++ Desktop/Intern Tasks/Shiprocket/app/src/App.jsx
@@ -0,0 +1,19 @@
+import React,{useState,useRef} from 'react'
+import Features from './components/Features'
+import Navbar from './components/Navbar'
+
+import Navbar2 from './components/Navbar2'
+import HomePage from './components/HomePage'
+
+function App() {
+
+  return (
+  <>
+<HomePage />
+
+
+  </>
+  )
+}
+
+export default App
